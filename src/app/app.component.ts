627a8106c1511d70c00d91317c711ac142ce71ef
SinanDmrr <sinan-demir@hotmail.com>
2025-01-20 15:05:19 +0100

    About me section was created: - Circle objects have been created and positioned - info div was created for the headline and the icons - aboutme div was created for the about me text
    In the header I linked to the id of the aboutme section via href and tested it, it works without errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,36 @@ import { RouterOutlet } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    TranslatePipe,
+    TranslateDirective,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Sinan Demir Frontend Developer';
 
-  constructor(private titleService: Title) {
+  constructor(
+    private translate: TranslateService,
+    private titleService: Title,
+  ) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+
     this.titleService.setTitle(this.title);
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideTranslateService({
+      defaultLanguage: 'en',
+    }),
+  ],
 };
