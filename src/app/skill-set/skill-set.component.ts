036b8222101e407356a45d347f7be4ec55e22896
SinanDmrr <sinan-demir@hotmail.com>
2025-01-21 18:21:45 +0100

    Complete page has been revised. - Header now floats above the above the fold section without an extra div - Icons in the above the fold section now move to the left when hovering - About me section has been created and hover effects for the icons and the button have been created - Skill set section was created here an object was created in the ts file with the icons and the text and this is then rendered into the HTML with the @for loop, furthermore a special hover effect was added to the last-child() that does not work like the other hover elements

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -5,8 +5,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
+  skills: InterfaceSkill[] = [
+    { img: '../../assets/images/angular-icon.png', name: 'Angular' },
+    { img: '../../assets/images/typescript-icon.png', name: 'TypeScript' },
+    { img: '../../assets/images/javascript.icon.png', name: 'JavaScript' },
+    { img: '../../assets/images/html5-icon.png', name: 'HTML' },
+    { img: '../../assets/images/css3-icon.png', name: 'CSS' },
+    { img: '../../assets/images/scss-icon.png', name: 'SCSS' },
+    {
+      img: '../../assets/images/material-design-icon.png',
+      name: 'Material Design',
+    },
+    { img: '../../assets/images/csharp-icon.png', name: 'C-Sharp' },
+    { img: '../../assets/images/rest-api-icon.png', name: 'REST API' },
+    { img: '../../assets/images/git-icon.png', name: 'GIT' },
+    { img: '../../assets/images/github-icon.png', name: 'GitHub' },
+    { img: '../../assets/images/firebase-icon.png', name: 'Firebase' },
+    { img: '../../assets/images/sql-icon.png', name: 'SQL' },
+    { img: '../../assets/images/vscode-icon.png', name: 'VS Code' },
+    { img: '../../assets/images/scrum-icon.png', name: 'SCRUM' },
+    { img: '../../assets/images/kanban-icon.png', name: 'Kanban' },
+    { img: '../../assets/images/grow-icon.png', name: 'Growth mindset' },
+  ];
+}
 
+interface InterfaceSkill {
+  img: string;
+  name: string;
 }
