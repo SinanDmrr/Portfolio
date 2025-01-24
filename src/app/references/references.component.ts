93f6ac851d69ba4697e3d1ed696afeddd05400a6
SinanDmrr <sinan-demir@hotmail.com>
2025-01-24 13:57:44 +0100

    References section was created Background color goes over 100% display width References consist of name, project name, feedback References were created flexibly via flex-wrap so that they automatically change position under one another depending on the display size

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
-export class ReferencesComponent {
-
-}
+export class ReferencesComponent {}
